c613e39f9b2267a21c8a6d07bcf794d3908bce77
rokhaya69 <54942212+rokhaya69@users.noreply.github.com>
2023-07-07 10:56:27 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+$nav = $('.nav');
+$toggleCollapse = $('.toggle-collapse');
+
+/**Click event on toggle menu */
+$toggleCollapse.click(function(){
+    $nav.toggleClass('collapse');
+})
+
+
+//Owl Carousel for blog
+$owl = $('.owl-carousel');
+$owl.owlCarousel({
+    loop:true,
+    autoplay:false,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:true,
+    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+});
+
+
+//click to scroll top
+$('.move-up span').click(function(){
+    $('html, body').animate({
+        scrollTop:0
+    },1000);
+})
+
+});
+$(document).ready(function(){
+    $('#birth-date').mask('00/00/0000');
+    $('#phone-number').mask('0000-0000');
+   })
